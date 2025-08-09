84c30cd2585379e050124a2982b2a894bcf81768
jongik96 <pji3504@naver.com>
2025-08-09 17:39:22 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-// pages/index.js
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 const IndexPage = () => {
   const router = useRouter();
@@ -10,7 +10,21 @@ const IndexPage = () => {
     router.push('/kansaiben');
   }, [router]);
 
-  return null; // 리다이렉트가 완료되면 아무 것도 렌더링하지 않음
+  return (
+    <>
+      <Head>
+        {/* SNS에서 공유될 때 썸네일 설정 */}
+        <meta property="og:image" content="/images/ben.png" />
+        <meta property="og:title" content="サトリ変換" />
+        <meta property="og:description" content="日本語を関西弁、博多弁、名古屋弁に変換します" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/images/ben.png" />
+        <meta name="twitter:title" content="サトリ変換" />
+        <meta name="twitter:description" content="日本語を関西弁、博多弁、名古屋弁に変換します" />
+      </Head>
+    </>
+  );
 };
 
 export default IndexPage;
+s
